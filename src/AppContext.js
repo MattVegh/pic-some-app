aa98b7dbd1c15e87405f04944054cb7eec76f1ae
Matt Vegh <mdv240@gmail.com>
2020-04-13 22:38:20 -0500

    fetching pics

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,14 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 const AppContext = React.createContext()
 
 function AppContextProvider(props) {
-    const [ pics, setPics ] = useState([])
+    const [pics, setPics] = useState([])
 
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
+            .then(response => response.json())
+            .then((allPics) => {
+                setPics([allPics])
+            })
+
+    }, [])
+
+
+
+    console.log(pics)
     return (
-        <AppContext.Provider value={{pics, setPics}}>
-            { props.children }
+        <AppContext.Provider value={{ pics, setPics }}>
+            {props.children}
         </AppContext.Provider>
     )
 }
 
-export { AppContextProvider, AppContext}
+export { AppContextProvider, AppContext }

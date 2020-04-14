be2463c04ededd5023789da17681f489948cae7c
Matt Vegh <mdv240@gmail.com>
2020-04-13 22:28:41 -0500

    adding state to context

--- src/AppContext.js
+++ src/AppContext.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 const AppContext = React.createContext()
 
 function AppContextProvider(props) {
-    const [ pics, setPics ] = useState('')
+    const [ pics, setPics ] = useState([])
 
     return (
-        <AppContext.Provider value={pics, setPics}>
+        <AppContext.Provider value={{pics, setPics}}>
             { props.children }
         </AppContext.Provider>
     )

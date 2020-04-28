a741f62808e7f492e49f83e6993d9ad40037843d
Matt Vegh <mdv240@gmail.com>
2020-04-28 10:53:28 -0500

    hovered

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Image({className, img}) {
+    const [ hovered, setHovered ] = useState(false)
+
+    console.log(hovered)
     return (
-        <div className={`${className} image-container`}>
+        <div className={`${className} image-container`}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             <img src={img.url} className="image-grid"/>
         </div>
     )

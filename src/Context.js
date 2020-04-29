7ce12b542290b9d6138e0171fe2b9c5c6346a950
Matt Vegh <mdv240@gmail.com>
2020-04-28 20:58:01 -0500

    toggleFavorite working

--- src/Context.js
+++ src/Context.js
@@ -1,31 +1,34 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 
 const Context = React.createContext()
 
-function ContextProvider({children}) {
+function ContextProvider({ children }) {
     const [pics, setPics] = useState([])
-    
+
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(data => setPics(data))
     }, [])
-    
+
     function toggleFavorite(id) {
-        const updatedPicArr = allPhotos.map(photo => {
-            if(photo.id === id) {
+        const updatedPicArr = pics.map(photo => {
+            if (photo.id === id) {
+                console.log(id)
+                console.log(!photo.isFavorite)
                 return {
                     ...photo,
-                    isFavorited: !photo.isFavorite
+                    isFavorite: !photo.isFavorite
                 }
             }
             return photo
         })
+        setPics(updatedPicArr)
     }
 
     return (
-        <Context.Provider value={{pics}}>
+        <Context.Provider value={{ pics, toggleFavorite }}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
+import { Context } from '../Context'
 
 function Image({ className, img }) {
     const [hovered, setHovered] = useState(false)
+    const { toggleFavorite } = useContext(Context)
 
-    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const heartIcon = hovered && <i onClick={() => toggleFavorite(img.id)} className="ri-heart-line favorite"></i>
     const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
 
     return (

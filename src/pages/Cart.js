a97888822c4aa9f5a9e5ee9d35f5272c3b1a0291
Matt Vegh <mdv240@gmail.com>
2020-04-29 20:24:10 -0500

    starting to set up cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useContext } from "react"
+import { Context } from '../Context'
 
 function Cart() {
+    const { cartItems } = useContext(Context)
+
+    const cartItemElements = cartItems.map(item => (
+        <h2>{item.id}</h2>
+    ))
+
     return (
         <main className="cart-page">
             <h1>Check out</h1>
+            {cartItemElements}
         </main>
     )
 }

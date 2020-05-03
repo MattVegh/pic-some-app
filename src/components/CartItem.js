04b0c120438cf29efe1d2a5a8fe2746718e1ef31
Matt Vegh <mdv240@gmail.com>
2020-05-02 21:14:26 -0500

    hover fills trash icon

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Context } from '../Context'
 
 function CartItem({ item }) {
+    const [hovered, setHovered] = useState(false)
     const { removeFromCart } = useContext(Context)
+
+    function trashIcon() {
+        if (hovered) {
+            return <i
+                onClick={() => removeFromCart(item.id)}
+                className='ri-delete-bin-fill'
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            ></i>
+        } else {
+            return <i
+                onClick={() => removeFromCart(item.id)}
+                className='ri-delete-bin-line'
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            ></i>
+        }
+    }
     return (
         <div className='cart-item'>
-            <i onClick={() => removeFromCart(item.id)} className='ri-delete-bin-line'></i>
+            {trashIcon()}
             <img src={item.url} width='130px' />
             <p>$5.99</p>
         </div>
